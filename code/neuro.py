1c065193f1aacfb38feea227fe09ea8e5ace007f
NoOneZero <turdecarta@gmail.com>
2021-01-12 21:31:52 +0200

    Додано cross_crossover_one

--- code/neuro.py
+++ code/neuro.py
@@ -1,5 +1,7 @@
+import code.config as config
 import random
 import math
+import copy
 
 class Neuro:
     def __init__(self, neuro = None, mutant_power = 0):
@@ -78,21 +80,19 @@ class Neuro:
         return math.tanh(number)
 
 
-import random
-import code.config as config
-
 class Web:
     def __init__(self, layers = config.WEB_LAYERS, web = None, randomize = None):
         self.layers = layers
         self.activation_function = lambda number: number
         if web != None:
-            self.neurons = [[web.neurons[i][j] for j in range(layers[i])] for i in range(len(layers))]
-            self.axon_weigh = [[web.axon_weigh[i][j] for j in range(layers[i] * layers[i + 1])] for i in range(len(layers) - 1)]
+            self.layers = copy.copy(web.layers)
+            self.neurons = [[web.neurons[i][j] for j in range(self.layers[i])] for i in range(len(self.layers))]
+            self.axon_weigh = [[web.axon_weigh[i][j] for j in range(self.layers[i] * self.layers[i + 1])] for i in range(len(self.layers) - 1)]
             self.axon_bias = [web.axon_bias[i] for i in range(len(web.axon_bias))]
         else:
-            self.neurons = [[0 for j in range(layers[i])] for i in range(len(layers))]
-            self.axon_weigh = [[0 for j in range(layers[i] * layers[i + 1])] for i in range(len(layers) - 1)]
-            self.axon_bias = [0 for i in range(len(layers) - 1)]
+            self.neurons = [[0 for j in range(self.layers[i])] for i in range(len(self.layers))]
+            self.axon_weigh = [[0 for j in range(self.layers[i] * self.layers[i + 1])] for i in range(len(self.layers) - 1)]
+            self.axon_bias = [0 for i in range(len(self.layers) - 1)]
         if randomize != None: self.randomize(randomize)
 
 
@@ -129,22 +129,37 @@ class Web:
         self.activation_function = activation_function
 
 
-    def crossover_one(neuro_0, neuro_1):
-        new_neuro = Neuro(neuro_0)
+
+    def crossover_one(self, neuro_1):
+        new_neuro = Web(web = self)
         sum = 0
         for i in range(len(new_neuro.axon_weigh)):
             for j in range(len(new_neuro.axon_weigh[i])):
-                for k in range(len(new_neuro.axon_weigh[i])):
-                    sum += 1
-        point = random.randint(1, sum)
+                sum += 1
+            sum += 1
+
+        point = random.randint(0, sum - 1)
         counter = 0
-        for i in range(len(new_neuro.axon_weigh)):
-            for j in range(len(new_neuro.axon_weigh[i])):
-                for k in range(len(new_neuro.axon_weigh[i][j])):
+        if random.random()<0.5:
+            for i in range(len(new_neuro.axon_weigh)):
+                for j in range(len(new_neuro.axon_weigh[i])):
                     if counter > point:
-                        new_neuro.axon_weigh[i][j][k] = neuro_1.axon_weigh[i][j][k]
+                        new_neuro.axon_weigh[i][j] = neuro_1.axon_weigh[i][j]
                     counter += 1
+                if counter > point:
+                    new_neuro.axon_bias[i] = neuro_1.axon_bias[i]
+                counter += 1
+        else:
+            for i in range(len(new_neuro.axon_weigh)):
+                for j in range(len(new_neuro.axon_weigh[i])):
+                    if counter < point:
+                        new_neuro.axon_weigh[i][j] = neuro_1.axon_weigh[i][j]
+                    counter += 1
+                if counter < point:
+                    new_neuro.axon_bias[i] = neuro_1.axon_bias[i]
+                counter += 1
         return new_neuro
+
     def randomize_new(neuro_0, neuro_1):
         new_neuro = Neuro(neuro_0)
         for i in range(len(new_neuro.axon_weigh)):
@@ -154,6 +169,9 @@ class Web:
                         new_neuro.axon_weigh[i][j][k] = neuro_1.axon_weigh[i][j][k]
         return new_neuro
 
+    # cross_crossover_one, cross_crossover_two, cross_crossover_several, cross_crossover_multi_several, cross_crossover_cycle
+    # cross_average, cross_lineral, cross_discret, cross_interjacent
+    # bias
 
     def calculate_all(self, input):
         self._set_input(input)
@@ -173,18 +191,17 @@ class Web:
     def _get_output(self):
         return self.neurons[-1]
 
-web = Web(layers=[2, 3, 2], randomize = 1)
-web.axon_weigh = [[1, 1, 1, 1, 1, -1], [1, 2, -1, 0, 1, 1]]
-web.axon_bias = [1, -1]
+web = Web(layers=[2, 3, 2])
+web.axon_weigh = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]]
+web.axon_bias = [13, 14]
 # web.set_function(lambda number: math.tanh(number))
-print("neuron", web.neurons)
-print("axon", web.axon_weigh)
-print("bias",  web.axon_bias)
-print("result:", web.calculate_all([1, 1]))
-print("neuron", web.neurons)
+print("father", web.axon_weigh, web.axon_bias)
 
-w = web.new_mutant_deep_copy(0.5, 10)
-print("axon", web.axon_weigh)
-print("axon", w.axon_weigh)
-print("bias",  web.axon_bias)
-print("bias",  w.axon_bias)
+webM = Web(layers = [2, 3, 2])
+webM.axon_weigh = [[-1, -2, -3, -4, -5, -6], [-7, -8, -9, -10, -11, -12]]
+webM.axon_bias = [-13, -14]
+print("mother", webM.axon_weigh, webM.axon_bias)
+
+for i in range(1000):
+    child = web.crossover_one(webM)
+    print("child {}".format(i), child.axon_weigh, child.axon_bias)
